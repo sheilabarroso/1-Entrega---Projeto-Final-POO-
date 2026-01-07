b51425e9641a68dee4fda68f2b741fd1bf42d5df
Vitória Cavalcante Souza <vitoria.csouza@outlook.com>
2026-01-07 10:55:53 -0300

    Create carrinho.py

---
+++ scr/carrinho.py
@@ -0,0 +1,56 @@
+class Carrinho:
+    def __init__(self, id_carrinho: int, cliente: Cliente):
+        self.id = id_carrinho
+        self.cliente = cliente
+        self.itens: Dict[str, ItemCarrinho] = {}
+
+    def adicionar_produto(self, produto: Produto, quantidade: int = 1):
+        if not produto.ativo:
+            raise ValueError("Produto inativo")
+
+       if quantidade > produto.estoque:
+           raise ValueError("Estoque insuficiente")
+
+        if produto.sku in self.itens:
+            self.itens[produto.sku].quantidade += quantidade
+        else:
+            self.itens[produto.sku] = ItemCarrinho(produto, quantidade)
+
+    def remover_produto(self, sku: str, quantidade: int = 1):
+        if sku not in self.itens:
+            raise ValueError("Produto não está no carrinho")
+
+        item = self.itens[sku]
+
+        if quantidade >= item.quantidade:
+            item.produto.repor_estoque(item.quantidade)
+            del self.itens[sku]
+        else:
+            item.quantidade -= quantidade
+            item.produto.repor_estoque(quantidade)
+
+    def limpar(self):
+        for item in self.itens.values():
+            item.produto.repor_estoque(item.quantidade)
+        self.itens.clear()
+
+    def total(self) -> float:
+        return sum(item.subtotal() for item in self.itens.values())
+
+    def exibir(self):
+
+        print(f"\n--- Carrinho #{self.id} | Cliente: {self.cliente.nome} ---")
+
+        if not self.itens:
+
+            print("Carrinho vazio.")
+            return
+
+        for item in self.itens.values():
+
+            # Formatted the print statement for better readability
+            print (
+                f"{item.produto.nome} ({item.quantidade}x) R$ {item.produto.preco_unitario:.2f} = R$ {item.subtotal():.2f}"
+            )
+
+        print (f"TOTAL: R$ {self.total():.2f}\n")
