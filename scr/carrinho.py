73ce5b02aa30cad131582a099b542a69f1ff5d84
Vitória Cavalcante Souza <vitoria.csouza@outlook.com>
2026-01-07 11:31:57 -0300

    Update carrinho.py

--- scr/carrinho.py
+++ scr/carrinho.py
@@ -1,8 +1,10 @@
+from item_carrinho import ItemCarrinho
+
 class Carrinho:
     def __init__(self, id_carrinho: int, cliente: Cliente):
         self.id = id_carrinho
         self.cliente = cliente
-        self.itens: Dict[str, ItemCarrinho] = {}
+        self.itens = {}
 
     def adicionar_produto(self, produto: Produto, quantidade: int = 1):
         if not produto.ativo:
@@ -16,40 +18,28 @@ class Carrinho:
         else:
             self.itens[produto.sku] = ItemCarrinho(produto, quantidade)
 
-    def remover_produto(self, sku: str, quantidade: int = 1):
+   def remover_produto(self, sku: str):
         if sku not in self.itens:
             raise ValueError("Produto não está no carrinho")
+        del self.itens[sku]
 
-        item = self.itens[sku]
-
-        if quantidade >= item.quantidade:
-            item.produto.repor_estoque(item.quantidade)
-            del self.itens[sku]
-        else:
-            item.quantidade -= quantidade
-            item.produto.repor_estoque(quantidade)
-
-    def limpar(self):
-        for item in self.itens.values():
-            item.produto.repor_estoque(item.quantidade)
-        self.itens.clear()
-
-    def total(self) -> float:
+    def total(self):
         return sum(item.subtotal() for item in self.itens.values())
 
-    def exibir(self):
+    def _len_(self):
+        return sum(item.quantidade for item in self.itens.values())
 
+    def exibir(self):
         print(f"\n--- Carrinho #{self.id} | Cliente: {self.cliente.nome} ---")
 
         if not self.itens:
-
             print("Carrinho vazio.")
             return
 
         for item in self.itens.values():
-
-            print (
-                f"{item.produto.nome} ({item.quantidade}x) R$ {item.produto.preco_unitario:.2f} = R$ {item.subtotal():.2f}"
+            print(
+                f"{item.produto.nome} ({item.quantidade}x) "
+                f"R$ {item.produto.preco_unitario:.2f} = R$ {item.subtotal():.2f}"
             )
 
-        print (f"TOTAL: R$ {self.total():.2f}\n")
+        print(f"TOTAL: R$ {self.total():.2f}\n")
